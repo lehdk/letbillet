bede281732d23395541e6a79a90661519614f1d9
Lehdk\Lehdk <leh99dk.mail@gmail.com>
2023-03-16 13:56:27 +0100

    Added fontawesome and an exit button for login

--- src/main/java/dk/letbillet/presentation/controller/LoginController.java
+++ src/main/java/dk/letbillet/presentation/controller/LoginController.java
@@ -1,8 +1,36 @@
 package dk.letbillet.presentation.controller;
 
-public class LoginController {
+import de.jensd.fx.glyphs.GlyphsDude;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcons;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.text.Text;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController implements Initializable {
+
+    @FXML
+    public Button exitButton;
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        try {
+            Text icon = GlyphsDude.createIcon(FontAwesomeIcons.CLOSE, "20pt");
+            icon.setStyle("-fx-font-family: FontAwesome; -fx-fill: RED;");
+
+            exitButton.setGraphic(icon);
+            exitButton.setText("");
+        } catch (Exception ignored) {
+
+        }
+
+    }
 
     public void handleExit() {
         System.exit(0);
     }
+
 }
