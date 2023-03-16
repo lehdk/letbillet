c51c0f8e332c3e9030700b7e623dc47c2776f849
Lehdk\Lehdk <leh99dk.mail@gmail.com>
2023-03-16 13:40:30 +0100

    Made FXML for login

--- src/main/java/dk/letbillet/Main.java
+++ src/main/java/dk/letbillet/Main.java
@@ -1,24 +1,40 @@
 package dk.letbillet;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
 
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
-    public void start(Stage primaryStage) throws Exception {
-        var loader = new FXMLLoader(getClass().getResource("presentation/view/Application.fxml"));
+    public void start(Stage stage) throws Exception {
+        var loader = new FXMLLoader(getClass().getResource("presentation/view/Login.fxml"));
 
         Parent root = loader.load();
-        var scene = new Scene(root, 1280, 720);
+        root.setOnMousePressed(event -> {
+            xOffset = event.getSceneX();
+            yOffset = event.getSceneY();
+        });
+        root.setOnMouseDragged(event -> {
+            stage.setX(event.getScreenX() - xOffset);
+            stage.setY(event.getScreenY() - yOffset);
+        });
+        var scene = new Scene(root);
 
-        primaryStage.setScene(scene);
-        primaryStage.show();
+        stage.setScene(scene);
+        stage.initStyle(StageStyle.UNDECORATED);
+        stage.setResizable(false);
+        stage.show();
     }
 }
---
+++ src/main/java/dk/letbillet/presentation/controller/LoginController.java
@@ -0,0 +1,8 @@
+package dk.letbillet.presentation.controller;
+
+public class LoginController {
+
+    public void handleExit() {
+        System.exit(0);
+    }
+}
